3ac060abb1a534b131e313e8989f270f6ad41e48
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-06-18 05:11:07 +0530

    Functions

---
+++ src/Functions.kt
@@ -0,0 +1,13 @@
+fun main(){
+    square(10)
+}
+fun square(num:Int):Int{
+    return num*num
+}
+fun square2(num:Int):Int=num*num
+fun square3(num:Int)=if(num>0)num*num else -1
+fun square4(num:Int)=num*num
+
+fun thisShouldStartWithLowerCase(){
+    //this is lowerCamelCase
+}
