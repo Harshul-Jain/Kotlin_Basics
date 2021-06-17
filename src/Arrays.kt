c99e43f6de2f5b1dd95b594e566969e55ccbfd7d
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-06-17 20:17:43 +0530

    Lists or Collections

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -9,7 +9,21 @@ val nameString= arrayOf<String>("1","2","3","4","5")
 val num=Array(5,{i->i*1})
 val numArray= intArrayOf(1,2)
 val nullArray= arrayOfNulls<String>(10)
+//Lists or Collections
+var listOfNames= listOf<String>("Hello","World","harshul")
+var mutableListOfNames= mutableListOf<String>("Hello ","World","harshul")
+var mutableArrayListOfNames= arrayListOf<String>("Hello ","World","harshul")
+val mapOfNames= mapOf<String,String>("Name" to "harshul","age" to "20","Nationality" to "Indian")
+//map is immutable while hashMap is mutable
+val hashMapOfNames= hashMapOf<String,String>("Name" to "harshul","age" to "20","Nationality" to "Indian")
+
+
 fun main(){
     println(num[0])
     println(num.get(0))
+    //listOfNames.add("harshul")   This does not works as list is immutable
+    mutableListOfNames.add("Jain")
+    println(mapOfNames["Name"])
+    hashMapOfNames["Occupation"]="Student"
+    println(hashMapOfNames["Occupation"])
 }
