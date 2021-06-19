f2d7507c79183b2f1bad8098a6fd57f07be852e0
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-06-20 02:21:20 +0530

    Constructors

--- src/classes.kt
+++ src/classes.kt
@@ -8,6 +8,10 @@ fun main(){
     account.withDraw(50F)
     println(account.amount)
     println("Account no ${account.accNo} ,Account Holder Name ${account.name} ,Amount ${account.amount}")
+    //Primary Constructor Declaration or Call
+    val accounts=Accounts(12345,"Harshul",1000f)
+    //Secondary Constructor Declaration or Call
+    val account3=Account3(12345)
 }
 
 //It is good Practice to declare class name first letter in UpperCase
@@ -28,7 +32,14 @@ class Account{
     var accNo:Int=0
     var name:String?=null
     var amount:Float=0f
-
+    //Constructor
+    /*
+    It is a function with same name as class name
+    It is used too construct or initialize the class
+    Two Type of Constructors
+    1.Primary Constructor-Only 1
+    2.Secondary Constructor-Multiple Secondary Constructorsgi
+     */
     //Member Functions
     /*fun insert(ac:Int,n:String,amnt:Float){
         accNo=ac
@@ -41,7 +52,6 @@ class Account{
         this.name=name
         this.amount=amount
     }
-
 
     fun deposit(money:Float){
         amount+=money
@@ -57,4 +67,57 @@ class Account{
         }
 
     }
+}
+//Primary Constructor
+class Accounts(var accNo:Int,var name:String,var amount:Float){
+
+}
+//Constructor with init block
+class Account2( accNo:Int, name:String, amount:Float){
+    var accNo:Int
+    var name:String
+    var amount:Float
+    init {
+        this.accNo=accNo
+        this.amount=amount
+        this.name=name
+    }
+}
+//Difference between Primary Constructor and Constructor with init block
+//in case of init block i can to manipulation which can not be done in other like
+/*
+ init {
+        this.accNo=accNo
+        this.amount=amount+1000f
+        this.name=name.capitalize()
+    }
+ */
+//In kotlin, const and val both represents the immutability and read only values and act as final keyword in java.
+// val keyword must be used to declare for run time values and const keyword must be used to declare compile time values.
+
+
+//Secondary Constructor
+class Account3{
+    var accNo:Int=1
+    var name:String=""
+    var amount:Float=0f
+
+    constructor(accNo :Int){
+        this.accNo=accNo
+        name="Default"
+        amount=0f
+    }
+
+    constructor(accNo: Int,name:String){
+        this.accNo=accNo
+        this.name=name
+        amount=0f
+    }
+
+    constructor(accNo: Int,name: String,amount: Float){
+        this.accNo=accNo
+        this.name=name
+        this.amount=amount
+    }
+
 }
