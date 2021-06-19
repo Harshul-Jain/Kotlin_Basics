5a760ad8179e8fab501631819f0dc82e0a3aaa0e
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-06-19 19:48:23 +0530

    Properties and Member Fn

---
+++ src/classes.kt
@@ -0,0 +1,53 @@
+fun main(){
+    val user:User=User()
+    user.email
+    val account=Account()
+    account.insert(12345,"Harshul",1000f)
+    account.deposit(100F)
+    account.withDraw(150F)
+    account.withDraw(50F)
+    println(account.amount)
+    println("Account no ${account.accNo} ,Account Holder Name ${account.name} ,Amount ${account.amount}")
+}
+
+//It is good Practice to declare class name first letter in UpperCase
+class User{
+    val name:String=""
+    val mobile:Int=1
+    val gender:String=""
+    val email:String=""
+    val bio:String=""
+
+    fun checkEmail():Boolean{
+        return email.isNotEmpty()
+    }
+
+}
+class Account{
+    //Properties
+    var accNo:Int=0
+    var name:String?=null
+    var amount:Float=0f
+
+    //Member Functions
+    fun insert(ac:Int,n:String,amnt:Float){
+        accNo=ac
+        name=n
+        amount=amnt
+    }
+
+    fun deposit(money:Float){
+        amount+=money
+        println(amount)
+    }
+    fun withDraw(money:Float){
+        if(amount<money){
+            println("Not sufficient Funds")
+        }
+        else{
+            amount-=money
+            println(amount)
+        }
+
+    }
+}
