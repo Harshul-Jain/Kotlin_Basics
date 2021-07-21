0d047197ce42f9d7a313972e652255506f7108f5
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-07-21 15:45:11 +0530

    ConcurrentModificationException

--- src/MultiThread.kt
+++ src/MultiThread.kt
@@ -4,16 +4,35 @@ fun main() {
     for (i in 0 until 10000) {
         numList.add(i)
     }
-    Thread { printList(1)}.start()
-    Thread { printList(2) }.start()
-    Thread { printList(3) }.start()
+//    Thread { printList(1 }.start()
+//    Thread { printList(2) }.start()
+//    Thread { printList(3) }.start
+    dropMultiples(3)
+    dropMultiples(5)
+    dropMultiples(7)
+    println(numList)
+    print("Done")
 }
 
 fun printList(id: Int) {
     for (i in 0 until 10000) {
-        if(id ==2 && i==500)
-            Thread.sleep(1000)
         if (i % 100 == 0)
-            println("$id  $i")
+            println("${Thread.currentThread().id}  $i")
     }
+}
+
+fun dropMultiples(n: Int) {
+    // causes ConcurrentModificationException
+//    for (i in  numList) {
+//        if (i % n == 0) {
+//            numList.remove(i)
+//        }
+//    }
+    // correctly
+    for (i in 0 until numList.size) {
+        if (i % n == 0) {
+            numList.remove(i)
+        }
+    }
+
 }
