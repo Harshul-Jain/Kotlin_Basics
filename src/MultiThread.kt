72c950d06c7dcd6fa51c010e0ecec41352107bdf
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-07-21 13:53:59 +0530

    Sleeping a Thread

--- src/MultiThread.kt
+++ src/MultiThread.kt
@@ -1,17 +1,19 @@
-val numList=ArrayList<Int>()
+val numList = ArrayList<Int>()
 
-fun main(){
-    for (i in 0 until 10000){
-       numList.add(i)
+fun main() {
+    for (i in 0 until 10000) {
+        numList.add(i)
     }
-    Thread{ printList()}.start()
-    Thread{ printList()}.start()
-    Thread{ printList()}.start()
+    Thread { printList(1)}.start()
+    Thread { printList(2) }.start()
+    Thread { printList(3) }.start()
 }
 
-fun printList() {
-    for(i in 0 until 10000){
-        if(i%100==0)
-        println("${Thread.currentThread().id}  $i")
+fun printList(id: Int) {
+    for (i in 0 until 10000) {
+        if(id ==2 && i==500)
+            Thread.sleep(1000)
+        if (i % 100 == 0)
+            println("$id  $i")
     }
 }
